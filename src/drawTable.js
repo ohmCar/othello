78bf4f75582b6895dc31aa2746273c2d4d9e1581
omkar mote <omkarmote210@gmail.com>
2017-12-28 14:38:32 +0530

    added default 4 coin images to othello table

--- src/drawTable.js
+++ src/drawTable.js
@@ -1,3 +1,4 @@
+let id=1;
 const generateTable=function(){
   let table=document.getElementById('table');
   for(let counter=0; counter < 8; counter++) {
@@ -10,9 +11,24 @@ const generateRows=function(table){
   let row=document.createElement('tr');
   for (let counter=0; counter < 8; counter++) {
     let cell=document.createElement('td');
+    cell.id=id++;
+    defaultImages(cell);
     row.appendChild(cell);
   };
   table.appendChild(row);
+  return;
+};
+
+const defaultImages=function(cell){
+  if(cell.id==28||cell.id==37){
+    let whiteCoin=document.createElement('img');
+    whiteCoin.src='whiteCoin.png';
+    cell.appendChild(whiteCoin);
+  } else if(cell.id==29||cell.id==36){
+    let blackCoin=document.createElement('img');
+    blackCoin.src='blackCoin.png';
+    cell.appendChild(blackCoin);
+  };
   return;
 };
 
